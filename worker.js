249a502b8b1dd7f0d3bfed3428d11bf0047f90eb
duankhoahoc <lenguyenngocthieu55@gmail.com>
2026-03-04 13:44:21 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -251,7 +251,7 @@ Format mẫu:
 ]`;
 
           const geminiResponse = await fetch(
-            `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${env.GEMINI_API_KEY}`,
+           `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash-lite:generateContent?key=${env.GEMINI_API_KEY}`,
             {
               method: "POST",
               headers: { "Content-Type": "application/json" },
@@ -430,7 +430,7 @@ Format mẫu:
 ]`;
 
           const geminiResponse = await fetch(
-            `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${env.GEMINI_API_KEY}`,
+            `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash-lite:generateContent?key=${env.GEMINI_API_KEY}`,
             {
               method: "POST",
               headers: { "Content-Type": "application/json" },
